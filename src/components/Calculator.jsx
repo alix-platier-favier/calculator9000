e2002238e5b1fb073391f2901b7d8890a1a27bf3
alixunderscore <alix.platier-favier@laplateforme.io>
2023-10-17 15:57:56 +0200

    Ajout it's over 9000

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,6 +5,7 @@ import NumberButton from './NumberButton';
 import OperatorButton from './OperatorButton';
 import EqualButton from './EqualButton';
 import ResetButton from './ResetButton';
+import ItSOverNineThousand from './ItSOverNineThousand';
 
 function Calculator() {
     const [currentOperation, setCurrentOperation] = useState('');
@@ -20,7 +21,8 @@ function Calculator() {
 
     function handleEqualClick() {
         try {
-            setResult(eval(currentOperation));
+            const Evaluate = result ? result + currentOperation : currentOperation;
+            setResult(eval(Evaluate));
             setCurrentOperation('');
         } catch(e) {
             setResult('Error');
@@ -28,19 +30,18 @@ function Calculator() {
     }
 
     function handleResetClick() {
-        try {
-            setResult(eval(currentOperation));
+        {
+            setResult('');
             setCurrentOperation('');
-        } catch(e) {
-            setResult('Error');
         }
     }
 
     return (
         <div className="calculator">
+            <ItSOverNineThousand value={result} />
             <BeautifulScreen currentOperation={currentOperation} result={result} />
             <div className="buttons w4">
-                {['1','2','3'].map(num => (
+                {['7','8','9'].map(num => (
                     <NumberButton key={num} value={num} onClick={handleNumberClick} />
                 ))}
                 {['/'].map(op => (
@@ -56,7 +57,7 @@ function Calculator() {
                 ))}
             </div>
             <div className="buttons w4">
-                {['7','8','9'].map(num => (
+                {['1','2','3'].map(num => (
                     <NumberButton key={num} value={num} onClick={handleNumberClick} />
                 ))}
                 {['-'].map(op => (
@@ -64,17 +65,17 @@ function Calculator() {
                 ))}
             </div>
             <div className="buttons w4">
-                    <NumberButton key={'0'} value={'0'} onClick={handleNumberClick} />
-                    {['.'].map(num => (
-                    <NumberButton key={num} value={num} onClick={handleNumberClick} />
-                    ))}
-                    <ResetButton onClick={handleEqualClick} />
-                    {['*'].map(op => (
-                    <OperatorButton key={op} value={op} onClick={handleOperatorClick} />
-                    ))}
+                <NumberButton key={'0'} value={'0'} onClick={handleNumberClick} />
+                {['.'].map(num => (
+                <NumberButton key={num} value={num} onClick={handleNumberClick} />
+                ))}
+                <ResetButton onClick={handleResetClick} />
+                {['*'].map(op => (
+                <OperatorButton key={op} value={op} onClick={handleOperatorClick} />
+                ))}
             </div>
             <div className="equals">
-                    <EqualButton onClick={handleEqualClick} />
+                <EqualButton onClick={handleEqualClick} />
             </div>
         </div>
     );
